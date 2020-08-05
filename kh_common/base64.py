8961e1a39a12732d9281fe6e36be81c8ad6d7f91
kheina <vixen@kheina.com>
2020-08-05 19:44:50 -0400

    add zero-padding base64 encoders

---
+++ kh_common/base64.py
@@ -0,0 +1,9 @@
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+
+
+def b64encode(bytestring) :
+	return urlsafe_b64encode(bytestring).strip(b'=')
+
+
+def b64decode(bytestring) :
+	return urlsafe_b64decode(bytestring + (4 - len(bytestring) % 4) * b'=')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='kh_common',
-	version='0.2.3',
+	version='0.2.4',
 	description='common libraries for kheina.com',
 	author='kheina',
 	url='https://gitlab.com/kheina.com/kh-common',
