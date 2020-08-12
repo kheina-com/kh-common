6b9b0783c158933e4313737556de5a98c6189e08
kheina <vixen@kheina.com>
2020-08-11 21:37:56 -0400

    add configs and use static hosts in auth

--- kh_common/auth.py
+++ kh_common/auth.py
@@ -2,22 +2,25 @@ from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
 from kh_common.http_error import Unauthorized
 from requests import post as requests_post
 from kh_common.caching import ArgsCache
+from kh_common.config import admin_host
 from kh_common.base64 import b64decode
 from time import time
 import ujson as json
 
 
+class AuthVersionUnknown(Exception) :
+	pass
+
+
 @ArgsCache(60 * 60 * 24)  # 24 hour cache
 def fetchPublicKey(key_id, algorithm) :
-	response = requests_post('http://127.0.0.1/v1/key', json={ 'key_id': key_id, 'algorithm': algorithm })
+	response = requests_post(f'{admin_host}/v1/key', json={ 'key_id': key_id, 'algorithm': algorithm })
 	return json.loads(response.text)
 
 
-def verifyToken(token) :
-	load, signature = tuple(map(b64decode, token.split('.')))
+def v1token(load, signature) :
 	version, algorithm, key_id, expires, guid, data = load.split(b'.', 5)
 
-	version = version.decode()
 	algorithm = algorithm.decode()
 	key_id = int.from_bytes(b64decode(key_id), 'big')
 	expires = int.from_bytes(b64decode(expires), 'big')
@@ -37,4 +40,23 @@ def verifyToken(token) :
 	except :
 		raise Unauthorized('Key validation failed.')
 
-	return json.loads(data)
+	return {
+		'guid': guid,
+		'expires': expires,
+		'data': json.loads(data),
+	}
+
+
+tokenVersionSwitch = {
+	1: v1token,
+}
+
+
+def verifyToken(token) :
+	load, signature = tuple(map(b64decode, token.split('.')))
+	version = int(a[:a.find('.')])
+
+	if version in tokenVersionSwitch :
+		return tokenVersionSwitch[version]
+
+	raise ValueError('The given token uses a version that is unable to be decoded.')
---
+++ kh_common/config.py
@@ -0,0 +1,27 @@
+from os import environ
+
+
+environment = environ.get('ENVIRONMENT', 'LOCAL').lower()
+
+local = {
+	'admin_host': 'http://127.0.0.1',
+}
+
+dev = {
+	'admin_host': 'https://auth-dev.kheina.com',
+}
+
+prod = {
+	'admin_host': 'https://auth.kheina.com',
+}
+
+env_vars = local
+if environment == 'prod' :
+	env_vars = prod
+elif environment == 'dev' :
+	env_vars = dev
+elif environment == 'local' :
+	env_vars = local
+
+
+admin_host = env_vars.get('admin_host')
