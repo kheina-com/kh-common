7e193bbdfd75af90a77a667552da4b400fe95f14
kheina <vixen@kheina.com>
2020-08-12 10:18:40 -0400

    update config file as well as change auth func to match new structure

--- kh_common/auth.py
+++ kh_common/auth.py
@@ -2,25 +2,22 @@ from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
 from kh_common.http_error import Unauthorized
 from requests import post as requests_post
 from kh_common.caching import ArgsCache
-from kh_common.config import admin_host
 from kh_common.base64 import b64decode
+from kh_common.config import auth_host
 from time import time
 import ujson as json
 
 
-class AuthVersionUnknown(Exception) :
-	pass
-
-
 @ArgsCache(60 * 60 * 24)  # 24 hour cache
 def fetchPublicKey(key_id, algorithm) :
-	response = requests_post(f'{admin_host}/v1/key', json={ 'key_id': key_id, 'algorithm': algorithm })
+	response = requests_post(f'{auth_host}/v1/key', json={ 'key_id': key_id, 'algorithm': algorithm })
 	return json.loads(response.text)
 
 
-def v1token(load, signature) :
+def v1token(token) :
 	version, algorithm, key_id, expires, guid, data = load.split(b'.', 5)
 
+	version = version.decode()
 	algorithm = algorithm.decode()
 	key_id = int.from_bytes(b64decode(key_id), 'big')
 	expires = int.from_bytes(b64decode(expires), 'big')
@@ -48,15 +45,14 @@ def v1token(load, signature) :
 
 
 tokenVersionSwitch = {
-	1: v1token,
+	b'1': v1token,
 }
 
 
 def verifyToken(token) :
-	load, signature = tuple(map(b64decode, token.split('.')))
-	version = int(load[:load.find(b'.')])
+	version = b64decode(token[:token.find(b'.')])
 
 	if version in tokenVersionSwitch :
-		return tokenVersionSwitch[version](load, signature)
+		return tokenVersionSwitch[version](token)
 
 	raise ValueError('The given token uses a version that is unable to be decoded.')
--- kh_common/config.py
+++ kh_common/config.py
@@ -4,16 +4,18 @@ from os import environ
 environment = environ.get('ENVIRONMENT', 'LOCAL').lower()
 
 local = {
-	'admin_host': 'http://127.0.0.1:5000',
+	'auth_host': 'http://127.0.0.1:5000',
 }
 
 dev = {
-	'admin_host': 'https://auth-dev.kheina.com',
+	'auth_host': 'https://auth-dev.kheina.com',
 }
 
 prod = {
-	'admin_host': 'https://auth.kheina.com',
+	'auth_host': 'https://auth.kheina.com',
 }
+
+assert local.keys() == dev.keys() == prod.keys()
 
 env_vars = local
 if environment == 'prod' :
@@ -24,4 +26,11 @@ elif environment == 'local' :
 	env_vars = local
 
 
-admin_host = env_vars.get('admin_host')
+# add the variables from the environment to the module
+locals().update(env_vars)
+
+# delete extraneous data
+del local, dev, prod, env_vars, environment, environ
+
+
+# put other variables/constants here (these will overwrite the env-specific configs above!)
