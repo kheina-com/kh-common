664d1f9a6bc486ae7da62c3829cc2f3a4ffd5ade
kheina <vixen@kheina.com>
2020-08-18 14:29:12 -0400

    add stringSlice func

--- kh_common/__init__.py
+++ kh_common/__init__.py
@@ -3,3 +3,10 @@ def getFullyQualifiedClassName(obj) :
 	if module and module != str.__module__ :
 		return f'{module}.{obj.__class__.__name__}'
 	return obj.__class__.__name__
+
+def stringSlice(string: str, start:str=None, end:str=None) :
+	if not string : return None
+	assert start or end, 'start or end is required'
+	start = string.rfind(start) + 1 if start else None
+	end = string.find(end) if end else None
+	return string[start:end]
