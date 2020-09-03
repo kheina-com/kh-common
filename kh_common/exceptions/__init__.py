80af78119dced8d447747cc776ed29fc81bef412
kheina <vixen@kheina.com>
2020-09-03 10:44:27 -0400

    update error handler to log as well

--- kh_common/exceptions/__init__.py
+++ kh_common/exceptions/__init__.py
@@ -1,11 +1,12 @@
 from kh_common import getFullyQualifiedClassName
 from starlette.responses import UJSONResponse
+from traceback import format_tb
 from sys import exc_info
 from uuid import uuid4
 
 
-def jsonErrorHandler(req, logger) :
-	e = exc_info()[1]
+def jsonErrorHandler(req, logger=None, stacktrace=False) :
+	e, exc_tb = exc_info()[1:]
 	status = getattr(e, 'status', 500)
 
 	error = {
@@ -15,6 +16,19 @@ def jsonErrorHandler(req, logger) :
 		'url': str(req.url),
 		'uuid': getattr(e, 'uuid', uuid4().hex),
 	}
+
+	traceback = format_tb(exc_tb)
+
+	if stacktrace :
+		error['stacktrace'] = traceback
+
+	if logger :
+		logger.error({
+			**error,
+			'stacktrace': traceback,
+			**getattr(e, 'logdata', { }),
+		})
+
 	return UJSONResponse(
 		error,
 		status_code=status,
