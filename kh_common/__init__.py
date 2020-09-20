eeeeec897f22fa5e55471d53a485c24f59b31845
kheina <46096341+kheina@users.noreply.github.com>
2020-09-20 12:13:20 -0400

    Add repo name function (#5)
    * add stringSlice func
    
    * add repo
    
    * increment version
    
    * some minor fixes

--- kh_common/__init__.py
+++ kh_common/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 
 
 def getFullyQualifiedClassName(obj) :
@@ -6,3 +6,11 @@ def getFullyQualifiedClassName(obj) :
 	if module and module != str.__module__ :
 		return f'{module}.{obj.__class__.__name__}'
 	return obj.__class__.__name__
+
+
+def stringSlice(string: str, start:str=None, end:str=None) :
+	if not string : return None
+	assert start or end, 'start or end is required'
+	start = string.rfind(start) + len(start) if start else None
+	end = string.find(end) if end else None
+	return string[start:end]
