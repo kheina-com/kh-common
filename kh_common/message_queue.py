40246da506d2a710449da8b7047e6fc2fd0a90d4
kheina <46096341+kheina@users.noreply.github.com>
2020-09-20 10:42:31 -0400

    Fix message queue crashing bug (#6)
    * fix crashing bug
    
    * iterate version
    
    * fix message queue not having connection during close
    
    * add init file to config
    
    * add json receiver

--- kh_common/message_queue.py
+++ kh_common/message_queue.py
@@ -1,17 +1,18 @@
+from kh_common.config.credentials import message_queue
 from kh_common import getFullyQualifiedClassName
 from traceback import format_tb
-import logging
+from kh_common import logging
 import pika
 import sys
 
 
 class Receiver :
 
-	def __init__(self, route, connection_info, channel_info, exchange_info=None) :
-		self._route = route
-		self._connection_info = connection_info
-		self._channel_info = channel_info
-		self._exchange_info = exchange_info
+	def __init__(self) :
+		self._route = message_queue['routing_key']
+		self._connection_info = message_queue['connection_info']
+		self._channel_info = message_queue['channel_info']
+		self._exchange_info = message_queue.get('exchange_info')
 		self.logger = logging.getLogger(__name__)
 
 
@@ -20,22 +21,32 @@ class Receiver :
 
 
 	def receiveAll(self) :
-		return [item for item in self._recv()]
+		return list(self._recv())
+
+
+	def receiveJson(self, forcelist=False) :
+		if forcelist :
+			return list(map(json.loads, self._recv()))
+		else :
+			return map(json.loads, self._recv())
 
 
 	def _recv(self) :
-		# returns a list of all messages retrieved from the message queue
-		connection = pika.BlockingConnection(pika.ConnectionParameters(**self._connection_info))
-		channel = connection.channel()
-		if self._exchange_info :
-			channel.exchange_declare(**self._exchange_info)
-			name = channel.queue_declare(self._route).method.queue
-			channel.queue_bind(routing_key=self._route, queue=name, exchange=self._exchange_info['exchange'])
-		else :
-			channel.queue_declare(self._route)
-			name = self._route
-
+		connection = None
 		try :
+			# returns a list of all messages retrieved from the message queue
+			connection = pika.BlockingConnection(pika.ConnectionParameters(**self._connection_info))
+			channel = connection.channel()
+
+			if self._exchange_info :
+				channel.exchange_declare(**self._exchange_info)
+				name = channel.queue_declare(self._route).method.queue
+				channel.queue_bind(routing_key=self._route, queue=name, exchange=self._exchange_info['exchange'])
+
+			else :
+				channel.queue_declare(self._route)
+				name = self._route
+
 			it = channel.consume(name, **self._channel_info)
 
 			ack = -1
@@ -45,12 +56,17 @@ class Receiver :
 					ack = max(ack, method_frame.delivery_tag)
 				else :
 					break
+
 			if ack >= 0 :
 				channel.basic_ack(delivery_tag=ack, multiple=True)
+
 			channel.cancel()
 		finally :
 			# don't channel.cancel here since, if it fails, we want the messages to remain in the queue
-			try : connection.close()
+			try :
+				if connection :
+					connection.close()
+
 			except :
 				exc_type, exc_obj, exc_tb = sys.exc_info()
 				self.logger.warning({
