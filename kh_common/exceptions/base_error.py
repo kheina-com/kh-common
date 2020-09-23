b717cdd9191d0071c1a89ac245316a7db72db9fb
kheina <vixen@kheina.com>
2020-09-23 19:08:14 -0400

    add another way of pulling refid from base error

--- kh_common/exceptions/base_error.py
+++ kh_common/exceptions/base_error.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 class BaseError(Exception) :
 	def __init__(self, message: str, *args:Tuple[Any], refid:Union[str, type(None)]=None, logdata:Dict[str, Any]={ }, **kwargs: Dict[str, Any]) -> type(None) :
 		Exception.__init__(self, message)
-		self.refid: str = refid or uuid4().hex
+		self.refid: str = refid or logdata.get('refid') or uuid4().hex
 		self.logdata: Dict[str, Any] = {
 			**logdata,
 			**kwargs,
