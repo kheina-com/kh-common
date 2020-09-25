429475e4f62d5d36ed59d66be33abef72a54a5e5
kheina <46096341+kheina@users.noreply.github.com>
2020-09-24 20:16:09 -0400

    Fix Tuple Sql Queries (#16)
    * fix
    
    * add hashable class
    
    * use getFullyQualifiedClassName
    
    * pass in arg converts
    
    * add typing

---
+++ kh_common/hashing.py
@@ -0,0 +1,17 @@
+from kh_common import getFullyQualifiedClassName
+from hashlib import sha1
+from math import ceil
+
+
+class Hashable :
+
+	_hash = 0
+
+	def __init__(self) :
+		name = f"<class '{getFullyQualifiedClassName(self)}' {Hashable._hash}>"
+		self.hash = int.from_bytes(sha1(name.encode()).digest(), 'big')
+		Hashable._hash += 1
+
+
+	def __hash__(self) :
+		return self.hash
