5bf409b86f1d173faecd09ea9aad10348a621cdf
kheina <vixen@kheina.com>
2020-09-24 20:13:06 -0400

    use getFullyQualifiedClassName

--- kh_common/hashing.py
+++ kh_common/hashing.py
@@ -1,3 +1,4 @@
+from kh_common import getFullyQualifiedClassName
 from hashlib import sha1
 from math import ceil
 
@@ -7,7 +8,7 @@ class Hashable :
 	_hash = 0
 
 	def __init__(self) :
-		name = f"<class '{self.__class__.__name__}' {Hashable._hash}>"
+		name = f"<class '{getFullyQualifiedClassName(self)}' {Hashable._hash}>"
 		self.hash = int.from_bytes(sha1(name.encode()).digest(), 'big')
 		Hashable._hash += 1
 
