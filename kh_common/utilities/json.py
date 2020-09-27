5894df860e68112adb2ff1c9ed5a9835932a66d0
kheina <vixen@kheina.com>
2020-09-26 23:36:40 -0400

    json streamy thingy

---
+++ kh_common/utilities/json.py
@@ -0,0 +1,21 @@
+from typing import Any, Callable, Dict, Iterable, List, Tuple, Union
+from datetime import datetime
+from decimal import Decimal
+
+
+_conversions: Dict[type, Callable] = {
+	datetime: datetime.timestamp,
+	tuple: list,
+	Decimal: float,
+}
+
+
+def _convert_item(self, item: Any) -> Any :
+	item_type = type(item)
+	if item_type in _conversions :
+		return _conversions[item_type](item)
+	return item
+
+
+def json_stream(stream: Iterable) :
+	map(_convert_item, stream)
