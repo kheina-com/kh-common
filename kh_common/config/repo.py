e1e12e56ab0e0a52503e7bf578a52ca8fbefaa09
kheina <vixen@kheina.com>
2020-11-15 16:30:27 -0500

    add tries so that non git repos don't break

--- kh_common/config/repo.py
+++ kh_common/config/repo.py
@@ -5,28 +5,37 @@ from kh_common import stringSlice
 
 name: Union[str, type(None)] = None
 
-output: Union[bytes, type(None)] = b''.join(Popen(['git', 'config', '--get', 'remote.origin.url'], stdout=PIPE, stderr=PIPE).communicate())
-if output and not output.startswith(b'fatal'):
-	name = stringSlice(output.decode(), '/', '.git')
-
-else :
-	output = b''.join(Popen(['git', 'rev-parse', '--show-toplevel'], stdout=PIPE, stderr=PIPE).communicate())
+try :
+	output: Union[bytes, type(None)] = b''.join(Popen(['git', 'config', '--get', 'remote.origin.url'], stdout=PIPE, stderr=PIPE).communicate())
 	if output and not output.startswith(b'fatal'):
-		name = stringSlice(output.decode(), '/').strip()
+		name = stringSlice(output.decode(), '/', '.git')
+
+	else :
+		output = b''.join(Popen(['git', 'rev-parse', '--show-toplevel'], stdout=PIPE, stderr=PIPE).communicate())
+		if output and not output.startswith(b'fatal'):
+			name = stringSlice(output.decode(), '/').strip()
+except :
+	pass
 
 
 short_hash: Union[str, type(None)] = None
 
-output = b''.join(Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=PIPE, stderr=PIPE).communicate())
-if output and not output.startswith(b'fatal'):
-	short_hash = output.decode().strip()
+try :
+	output = b''.join(Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=PIPE, stderr=PIPE).communicate())
+	if output and not output.startswith(b'fatal'):
+		short_hash = output.decode().strip()
+except :
+	pass
 
 
 full_hash: Union[str, type(None)] = None
 
-output = b''.join(Popen(['git', 'rev-parse', 'HEAD'], stdout=PIPE, stderr=PIPE).communicate())
-if output and not output.startswith(b'fatal'):
-	full_hash = output.decode().strip()
+try :
+	output = b''.join(Popen(['git', 'rev-parse', 'HEAD'], stdout=PIPE, stderr=PIPE).communicate())
+	if output and not output.startswith(b'fatal'):
+		full_hash = output.decode().strip()
+except :
+	pass
 
 
 del output, stringSlice, PIPE, Popen
