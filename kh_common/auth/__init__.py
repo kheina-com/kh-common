cd0e5b2247fab7e573845c5e5056b0b3cdd95739
kheina <vixen@kheina.com>
2021-02-13 20:51:08 -0500

    adding logging on token

--- kh_common/auth/__init__.py
+++ kh_common/auth/__init__.py
@@ -21,7 +21,7 @@ class KhUser(KhUser) :
 	def authenticated(self, raise_error=True) :
 		if not self.token or self.token != verifyToken(self.token.token_string) :
 			if raise_error :
-				raise Unauthorized('User is not authenticated.')
+				raise Unauthorized('User is not authenticated.', token=self.token)
 			return False
 		return True
 
