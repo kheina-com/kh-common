f7dad9ecc5750e3b6de04f46740bf02c80fb74b5
kheina <vixen@kheina.com>
2021-02-13 20:10:39 -0500

    make user a dict and not list

--- kh_common/utilities/json.py
+++ kh_common/utilities/json.py
@@ -3,6 +3,7 @@ from kh_common.models import KhUser
 from datetime import datetime
 from decimal import Decimal
 from enum import Enum
+from uuid import UUID
 
 
 _conversions: Dict[type, Callable] = {
@@ -11,23 +12,22 @@ _conversions: Dict[type, Callable] = {
 	tuple: lambda x : list(filter(None, x)),
 	list: lambda x : list(filter(None, x)),
 	Enum: lambda x : x.name,
+	UUID: lambda x : x.hex,
 	KhUser: lambda x : {
 		'user_id': x.user_id,
 		'scope': list(x.scope),
 		'token': {
 			'expires': x.token.expires,
-			'guid': x.token.guid,
+			'guid': x.token.guid.hex,
 			'data': x.token.data,
 		},
-	}
+	},
 }
 
 
 def _convert_item(item: Any) -> Any :
 	if isinstance(item, str) :
 		return item
-	if isinstance(item, Iterable) :
-		return json_stream(item)
 	for cls in type(item).__mro__ :
 		if cls in _conversions :
 			return _conversions[cls](item)
