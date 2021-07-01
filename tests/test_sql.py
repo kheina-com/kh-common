7b435ed2ce0a333d16020a4554a12aaa0694a7d9
kheina <vixen@kheina.com>
2021-07-01 19:02:44 -0400

    fix tests failing

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -1,3 +1,4 @@
+from kh_common.config import credentials; credentials.db = { }
 from kh_common.sql.query import Field, Join, JoinType, Operator, Order, Query, Table, Value, Where
 from typing import Any, List, Tuple, Union
 import pytest
