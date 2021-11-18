8687b3183790a73b0c4c51ac193637d2f799bbda
kheina <vixen@kheina.com>
2021-11-17 21:51:43 -0500

    revert timezonething

--- kh_common/datetime.py
+++ kh_common/datetime.py
@@ -1,9 +1,5 @@
-from pytz import all_timezones, common_timezones, timezone
 from datetime import timezone, datetime as pydatetime
 from typing import Union
-
-
-timezones = { tz._utcoffset: tz for tz in map(timezone, all_timezones + common_timezones) if tz._utcoffset.total_seconds() % 3600 == 0 }
 
 
 class datetime(pydatetime) :
@@ -14,7 +10,3 @@ class datetime(pydatetime) :
 
 	def now(timezone: timezone = timezone.utc) :
 		return pydatetime.now(timezone)
-
-
-	def normalize(datetime: pydatetime) :
-		return timezones[datetime.utcoffset()].normalize(datetime)
